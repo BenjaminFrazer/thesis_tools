a870a0f9eb1b4b4fd440a1b95d6e95de915071ff
benjaminf <benjamin.frazer.1997@gmail.com>
2022-09-07 23:21:23 +0100

    fixed util imports

--- thesis_tools/util/__init__.py
+++ thesis_tools/util/__init__.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env ipython
-from .plots import plot_sample_loadprofile
-from .plots import plot_sample_loadprofile_noshift
-from .plots import plotly_sample_load_profiles
-from .plots import plotly_sample_load_profiles_noshift
 from .process_results import combine_and_store_overal_temp_results
 from .process_results import combine_and_store_overal_pow_results
 from .data import Data
